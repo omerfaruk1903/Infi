ca34c2610b1ac602fb16413d5f48bb5bd85b16f9
omerfaruk1903 <omerfaruk1903.mfk@gmail.com>
2023-10-18 11:24:06 +0200

    ControlStructurs4 erstellt.

--- src/at/öfk/basis/ControlStructurs4.java
+++ src/at/öfk/basis/ControlStructurs4.java
@@ -1,17 +1,35 @@
 package at.öfk.basis;
 
+import java.util.Random;
 
 public class ControlStructurs4 {
+
     public static void main(String[] args) {
-        // Erstelle zwei Zufallszahl zwischen 0 und 100
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
 
-        // Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
-        // dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+        int randomNumber = random.nextInt(100);
+        int randomNumber1 = random.nextInt(100);
 
-        // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
-        // dann gib aus "Eine der beiden ist kleiner als 30"
+        System.out.println(randomNumber);
+        System.out.println(randomNumber1);
 
-        // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
-// dann gib aus "Erste Zahl klein, zweite kein 50iger"{
+        if((randomNumber<randomNumber1) && (randomNumber1>50)){
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+
+        } else if((randomNumber>30) && (randomNumber1>30)){
+            System.out.println("Eine der beiden ist kleiner als 30");
+
+        } else if (randomNumber>50 && randomNumber1!=50){
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+
+        }
+        // Gib die Zufallszahl aus
+
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+        // Wenn die Zahl größer als 50 ist gib aus "Large"
+
+
     }
 }
